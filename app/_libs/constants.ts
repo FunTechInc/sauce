9cccd88d1fa03fa6ca09c7889c61f81e7d871d8c
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2023-08-22 18:59:59 +0900

    8.22

--- app/_libs/constants.ts
+++ app/_libs/constants.ts
@@ -4,8 +4,11 @@ export const ISPROD = process.env.NODE_ENV === "production";
 export const ISCLIENT = typeof document !== "undefined";
 export const ISSERVER = !ISCLIENT;
 
-// export const SITEURL = new URL(process.env.NEXT_PUBLIC_SITE_URL!);
-// export const SITEORIGIN = SITEURL.origin;
+export const SITEURL = new URL(process.env.NEXT_PUBLIC_SITE_URL!);
+export const SITEORIGIN = SITEURL.origin;
+
+export const SITENAME = "(hidden)sauce";
+export const SITEDESCRIPTION = "this is our「秘伝のタレ」, means hidden sauce.";
 
 export const WINDOWSM = 560;
 export const WINDOWMD = 960;
--- app/font.ts
+++ app/font.ts
@@ -1,5 +1,9 @@
 import { Poppins } from "next/font/google";
-const poppins = Poppins({ subsets: ["latin"], weight: ["700"] });
+const poppins = Poppins({
+   subsets: ["latin"],
+   weight: ["700"],
+   variable: "--font-poppins",
+});
 import { Noto_Sans_JP } from "next/font/google";
 const noto = Noto_Sans_JP({ subsets: ["latin"], variable: "--font-noto" });
 
