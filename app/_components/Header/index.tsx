7e9e7888c6a477d1a8ef4b0db4db31ca1a8f0591
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2023-09-13 17:07:59 +0900

    headerのstyle修正

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,24 +1,27 @@
 import Link from "next/link";
 import { Menu } from "./components/Menu";
 import { HeaderWrapper } from "./components/HeaderWrapper";
+import { Inner } from "@/app/_layout/Inner";
 import s from "./header.module.scss";
 
 export const Header = () => {
    return (
       <HeaderWrapper>
-         <h1>
-            <Link className={s.link} href={"/"} scroll={false}>
-               (hidden)sauce
-            </Link>
-         </h1>
-         <Menu />
-         <nav>
-            <li>
-               <Link className={s.link} href={"/sample"} scroll={false}>
-                  sample
+         <Inner width="wide" className={s.inner}>
+            <h1>
+               <Link className={s.link} href={"/"} scroll={false}>
+                  (hidden)sauce
                </Link>
-            </li>
-         </nav>
+            </h1>
+            <Menu />
+            <nav>
+               <li>
+                  <Link className={s.link} href={"/sample"} scroll={false}>
+                     sample
+                  </Link>
+               </li>
+            </nav>
+         </Inner>
       </HeaderWrapper>
    );
 };
