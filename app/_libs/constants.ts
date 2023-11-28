356e0a900a7ece0a48a973c56d5cc9a317d58feb
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2023-11-28 19:49:07 +0900

    constants修正

--- app/_libs/constants.ts
+++ app/_libs/constants.ts
@@ -4,8 +4,19 @@ export const ISPROD = process.env.NODE_ENV === "production";
 export const ISCLIENT = typeof document !== "undefined";
 export const ISSERVER = !ISCLIENT;
 
-export const SITEURL = new URL(process.env.NEXT_PUBLIC_SITE_URL!);
+export const SITEURL = process.env.NEXT_PUBLIC_SITE_URL
+   ? new URL(process.env.NEXT_PUBLIC_SITE_URL)
+   : null;
+
+if (!SITEURL) {
+   throw new Error("NEXT_PUBLIC_SITE_URL is not set");
+}
 export const SITEORIGIN = SITEURL.origin;
+
+const DEV_URL = "http://localhost:3000/";
+// const DEV_URL = "http://192.168.100.26:3000/";
+
+export const HREF = ISDEV ? DEV_URL : SITEURL;
 
 export const SITENAME = "(hidden)sauce";
 export const SITEDESCRIPTION = "this is our「秘伝のタレ」, means hidden sauce.";
