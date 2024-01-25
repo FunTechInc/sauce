269a2d90baf8659d0935091de77a85861bc82646
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-01-25 20:35:51 +0900

    update Lenis

--- app/_hooks/useLenis/index.ts
+++ app/_hooks/useLenis/index.ts
@@ -2,7 +2,7 @@ import { useAppStore } from "@/app/_context/useAppStore";
 import Lenis from "@studio-freight/lenis";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { create } from "zustand";
 
 type AppStore = {
@@ -15,44 +15,35 @@ export const useLenis = create<AppStore>((set) => ({
    setLenis: (value: Lenis) => set({ lenis: value }),
 }));
 
-// const lenis = new Lenis({
-//    duration: 1.2,
-//    easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-//    smoothWheel: true,
-//    normalizeWheel: true,
-//    infinite: false,
-//    syncTouch: false,
-// });
-
 export const useLenisRegister = () => {
-   const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-      normalizeWheel: true,
-      infinite: false,
-      syncTouch: false,
-   });
-
-   useLenis.setState({ lenis });
+   const lenis = useRef<Lenis | null>(null);
+   const setLenis = useLenis((state) => state.setLenis);
 
    useEffect(() => {
-      if (!lenis) {
-         return;
-      }
+      lenis.current = new Lenis({
+         duration: 1.2,
+         easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+         smoothWheel: true,
+         normalizeWheel: true,
+         infinite: false,
+         syncTouch: false,
+      });
+
       gsap.registerPlugin(ScrollTrigger);
       ScrollTrigger.refresh();
-      lenis.on("scroll", ScrollTrigger.update);
-   }, []);
+      lenis.current.on("scroll", ScrollTrigger.update);
+
+      setLenis(lenis.current);
+   }, [setLenis]);
 
    const isModalOpen = useAppStore(({ isModalOpen }) => isModalOpen);
    const isMenuOpen = useAppStore(({ isMenuOpen }) => isMenuOpen);
 
    useEffect(() => {
       if (isModalOpen || isMenuOpen) {
-         lenis.stop();
+         lenis.current?.stop();
       } else {
-         lenis.start();
+         lenis.current?.start();
       }
    }, [isModalOpen, isMenuOpen]);
 
--- app/_layout/MainLayout/MainWrapper.tsx
+++ app/_layout/MainLayout/MainWrapper.tsx
@@ -1,24 +1,25 @@
 "use client";
 
-// import { useLenisRegister } from "@/app/_hooks/useLenis";
+import { useLenisRegister } from "@/app/_hooks/useLenis";
 import { WrapperLayout as MyWrapper } from "@funtech-inc/spice";
 import gsap from "gsap";
 import { useEffect } from "react";
 
 export const MainWrapper = ({ children }: { children: React.ReactNode }) => {
-   // const lenis = useLenisRegister();
+   const lenis = useLenisRegister();
 
-   // useEffect(() => {
-   //    if (!lenis) return;
-   //    function update(time: number) {
-   //       lenis.raf(time * 1000);
-   //    }
-   //    gsap.ticker.add(update);
-   //    gsap.ticker.lagSmoothing(0);
-   //    return () => {
-   //       gsap.ticker.remove(update);
-   //    };
-   // }, [lenis]);
+   // r3fのaddEffectでループを回したい時は、以下を削除して、canvasコンテキストで`useLenis`を使う
+   useEffect(() => {
+      if (!lenis.current) return;
+      function update(time: number) {
+         lenis.current?.raf(time * 1000);
+      }
+      gsap.ticker.add(update);
+      gsap.ticker.lagSmoothing(0);
+      return () => {
+         gsap.ticker.remove(update);
+      };
+   }, [lenis]);
 
    return <MyWrapper>{children}</MyWrapper>;
 };
