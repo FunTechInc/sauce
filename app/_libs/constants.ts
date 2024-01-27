c687dd417fad8852bb071dd2ea4329454e8455a3
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-01-27 12:27:26 +0900

    add comments

--- app/_libs/constants.ts
+++ app/_libs/constants.ts
@@ -1,9 +1,7 @@
 export const ISDEV = process.env.NODE_ENV === "development";
 export const ISPROD = process.env.NODE_ENV === "production";
-
 export const ISCLIENT = typeof document !== "undefined";
 export const ISSERVER = !ISCLIENT;
-
 export const SITEURL = process.env.NEXT_PUBLIC_SITE_URL
    ? new URL(process.env.NEXT_PUBLIC_SITE_URL)
    : null;
@@ -12,19 +10,26 @@ if (!SITEURL) {
    throw new Error("NEXT_PUBLIC_SITE_URL is not set");
 }
 export const SITEORIGIN = SITEURL.origin;
-
 const DEV_URL = "http://localhost:3000/";
 // const DEV_URL = "http://192.168.100.26:3000/";
-
 export const HREF = ISDEV ? DEV_URL : SITEURL;
 
+/*===============================================
+meta
+===============================================*/
 export const SITENAME = "(hidden)sauce";
 export const SITEDESCRIPTION = "this is our「秘伝のタレ」, means hidden sauce.";
 
+/*===============================================
+size
+===============================================*/
 export const WINDOWSM = 560;
 export const WINDOWMD = 960;
 export const WINDOWLG = 1120;
 
+/*===============================================
+animation
+===============================================*/
 export const TRIGGERTIMING = "top bottom-=10%";
 export const INTERSECTIONTIMING = "-10% 0px";
 export const DURATIONVAL = 0.8;
