8e630b72233bc35976b3f0efa9dbb1027d05af0d
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-04-18 19:57:45 +0900

    Improved accessibility of Menu component

--- app/[lang]/_components/Header/components/Menu/index.tsx
+++ app/[lang]/_components/Header/components/Menu/index.tsx
@@ -1,20 +1,38 @@
 "use client";
 
 import { useAppStore } from "@/app/[lang]/_context/useAppStore";
+import { useRef } from "react";
+import { FocusTrap } from "./FocusTrap";
+import { useCloseOnEscapeKeyDown } from "./useCloseOnEscapeKeyDown";
 import s from "./menu.module.scss";
 
 export const Menu = () => {
-   const isMenuOpen = useAppStore(({ isMenuOpen }) => isMenuOpen);
+   const { isMenuOpen, setIsMenuOpen } = useAppStore(
+      ({ isMenuOpen, setIsMenuOpen }) => ({ isMenuOpen, setIsMenuOpen })
+   );
+
+   useCloseOnEscapeKeyDown();
+
+   const buttonRef = useRef<HTMLButtonElement>(null);
    return (
       <>
          <button
+            ref={buttonRef}
             onClick={() => {
-               useAppStore.setState({ isMenuOpen: !isMenuOpen });
+               setIsMenuOpen(!isMenuOpen);
             }}
-            className={`${s.button} ${isMenuOpen ? s.active : ""}`}>
+            className={s.button}
+            aria-expanded={isMenuOpen}>
             {isMenuOpen ? "close" : "menu open"}
          </button>
-         <div className={`${s.inner} ${isMenuOpen ? "" : s.hidden}`}>menu</div>
+         <nav
+            className={`${s.nav} ${isMenuOpen ? "" : s.hidden}`}
+            area-hidden={`${!isMenuOpen}`}>
+            <button>nav0</button>
+            <button>nav1</button>
+            <button>nav2</button>
+            <FocusTrap focusTarget={buttonRef} />
+         </nav>
       </>
    );
 };
---
+++ app/[lang]/_components/Header/components/Menu/useCloseOnEscapeKeyDown.ts
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useAppStore } from "@/app/[lang]/_context/useAppStore";
+
+/** close menu when pressing the escape key */
+export const useCloseOnEscapeKeyDown = () => {
+   const { isMenuOpen, setIsMenuOpen } = useAppStore(
+      ({ isMenuOpen, setIsMenuOpen }) => ({ isMenuOpen, setIsMenuOpen })
+   );
+
+   useEffect(() => {
+      const handleKeydown = (e: KeyboardEvent) => {
+         if (e.key === "Escape" && isMenuOpen) {
+            console.log("close");
+            setIsMenuOpen(false);
+         }
+      };
+      window.addEventListener("keydown", handleKeydown);
+      return () => {
+         window.removeEventListener("keydown", handleKeydown);
+      };
+   }, [isMenuOpen, setIsMenuOpen]);
+};
