8d1da64d281a09790b180f3ae3f2ace406ac56c7
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-05-21 21:14:07 +0900

    add AutoScrollContainer & SplitText to utils

--- app/[lang]/(pages)/_home/index.tsx
+++ app/[lang]/(pages)/_home/index.tsx
@@ -1,7 +1,7 @@
 import { SampleLayout } from "@/app/[lang]/_layout/SampleLayout";
 import { Locale } from "@/i18n-config";
 import { getDictionary } from "@/app/[lang]/_libs/get-dictionary";
-import { AutoScrollContainer } from "@funtech-inc/spice";
+import { AutoScrollContainer } from "../../_utils/AutoScrollContainer";
 
 const HomeContent = async ({ lang }: { lang: Locale }) => {
    const dictionary = await getDictionary(lang);
