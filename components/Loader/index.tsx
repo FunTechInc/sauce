ec8375599a69d3b27c199ed644f90c4f741e3862
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-05-25 14:17:50 +0900

    fix:Video

--- components/Loader/index.tsx
+++ components/Loader/index.tsx
@@ -1,50 +1,34 @@
 "use client";
 
-import { VideoHTMLAttributes, useState } from "react";
+import { useState } from "react";
 import Image, { ImageProps } from "next/image";
-import { LowPowerVideo, LowPowerVideoProps } from "@funtech-inc/spice";
+import {
+   LowPowerVideo,
+   LowPowerVideoProps,
+   Video,
+   VideoProps,
+} from "@funtech-inc/spice";
 import { Loader } from "./Loader";
-import s from "./loader.module.scss";
+import s from "./index.module.scss";
 
-const FILL_STYLE = {
-   position: "absolute",
-   width: "100%",
-   height: "100%",
-   top: 0,
-   bottom: 0,
-   left: 0,
-   right: 0,
-   color: "transparent",
-} as React.CSSProperties;
-
-export const VideoLoader = (
-   props: VideoHTMLAttributes<HTMLVideoElement> & { fill?: boolean }
-) => {
+export const VideoLoader = (props: VideoProps) => {
    const [isLoaded, setIsLoaded] = useState(false);
-   const { fill, style, ...rest } = props;
-   const fillStyle = fill ? { ...FILL_STYLE, ...style } : style;
+   const { fill } = props;
    return (
       <div className={fill ? s.fillContainer : s.container}>
-         <video
-            {...rest}
-            style={fillStyle}
-            onCanPlay={() => setIsLoaded(true)}></video>
+         <Video {...props} onCanPlay={() => setIsLoaded(true)}></Video>
          {!isLoaded && <Loader delay={0} className={s.loader} />}
       </div>
    );
 };
 
-export const LowPowerVideoLoader = (
-   props: LowPowerVideoProps & { fill?: boolean }
-) => {
+export const LowPowerVideoLoader = (props: LowPowerVideoProps) => {
    const [isLoaded, setIsLoaded] = useState(false);
-   const { fill, style, ...rest } = props;
-   const fillStyle = fill ? { ...FILL_STYLE, ...style } : style;
+   const { fill } = props;
    return (
       <div className={fill ? s.fillContainer : s.container}>
          <LowPowerVideo
-            {...rest}
-            style={fillStyle}
+            {...props}
             onCanPlay={() => setIsLoaded(true)}></LowPowerVideo>
          {!isLoaded && <Loader delay={0} className={s.loader} />}
       </div>
