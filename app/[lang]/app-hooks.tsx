93c2cfe7715d7f2488497e1eb9dd38d10f89cc13
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-05-25 18:13:19 +0900

    update

--- app/[lang]/app-hooks.tsx
+++ app/[lang]/app-hooks.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { useEffect } from "react";
 import { useStarter } from "@funtech-inc/spice";
-import { useAppStore } from "./_context/useAppStore";
-import { useLenisRegister } from "@/hooks/useLenis";
 import { poppins, FF_EN } from "./font";
+import { useLenisRegister } from "@/hooks/useLenis";
+import { useFontsLoaded } from "@/hooks/useFontsLoaded";
 
 export const AppHooks = () => {
    useStarter({
@@ -22,37 +21,4 @@ export const AppHooks = () => {
          }
       `}</style>
    );
-};
-
-/**
- * set opacity to 1 after font is loaded
- */
-const useFontsLoaded = () => {
-   useEffect(() => {
-      const maxWaitTime = 1500;
-
-      const timeout = window.setTimeout(() => {
-         onReady();
-      }, maxWaitTime);
-
-      function onReady() {
-         window.clearTimeout(timeout);
-         useAppStore.setState({ fontsLoaded: true });
-         document.documentElement.classList.add("fonts-loaded");
-      }
-
-      try {
-         document.fonts.ready
-            .then(() => {
-               onReady();
-            })
-            .catch((error: unknown) => {
-               console.error(error);
-               onReady();
-            });
-      } catch (error) {
-         console.error(error);
-         onReady();
-      }
-   }, []);
 };
