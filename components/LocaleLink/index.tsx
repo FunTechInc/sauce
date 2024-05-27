976510821b6192ef390a5a4037d4eb30cafd5c69
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-05-27 14:09:27 +0900

    LocaleLinkをforwarding

--- components/LocaleLink/index.tsx
+++ components/LocaleLink/index.tsx
@@ -3,16 +3,25 @@
 import { useLenisLink } from "@/hooks/useLenis";
 import Link, { LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
+import { forwardRef } from "react";
 
-type LocaleLinkProps = LinkProps &
+export type LocaleLinkProps = LinkProps &
    React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
-export const LocaleLink = ({ children, href, ...props }: LocaleLinkProps) => {
-   const pathname = usePathname();
-   const lang = pathname.split("/")[1];
-   const localedHref = `/${lang}${href}`;
+export const LocaleLink = forwardRef<HTMLAnchorElement, LocaleLinkProps>(
+   ({ children, href, ...props }, ref) => {
+      const pathname = usePathname();
+      const lang = pathname.split("/")[1];
+      const localedHref = `/${lang}${href}`;
 
-   const bild = useLenisLink({ ...props, href: localedHref });
+      const bild = useLenisLink({ ...props, href: localedHref });
 
-   return <Link {...bild}>{children}</Link>;
-};
+      return (
+         <Link {...bild} ref={ref}>
+            {children}
+         </Link>
+      );
+   }
+);
+
+LocaleLink.displayName = "LocaleLink";
