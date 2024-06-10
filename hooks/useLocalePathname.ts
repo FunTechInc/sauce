38cb238f5cf626c0b0ef61f8f7ea994c622a1f39
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-06-10 13:03:07 +0900

    rename useLocalePathname

--- hooks/useLocalePathname.ts
+++ hooks/useLocalePathname.ts
@@ -4,6 +4,6 @@ export const useLocalePathname = ({ href = "" }: { href?: string } = {}) => {
    const pathname = usePathname();
    const lang = pathname.split("/")[1];
    const localeHref = `/${lang}${href}`;
-   const removeLocale = "/" + pathname.split("/").slice(2).join("/");
-   return { localeHref, pathname, lang, removeLocale };
+   const basePathname = "/" + pathname.split("/").slice(2).join("/");
+   return { localeHref, pathname, lang, basePathname };
 };
