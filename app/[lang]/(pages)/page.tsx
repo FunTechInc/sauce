a2eaa351cc4fbaac86e8d8728a27f23a2e6a89fc
takuma-hmng8 <t.hashimoto@funtech.inc>
2024-07-09 15:46:31 +0900

    add:pageTransition

--- app/[lang]/(pages)/page.tsx
+++ app/[lang]/(pages)/page.tsx
@@ -1,5 +1,5 @@
 import { Locale } from "@/i18n-config";
-import HomeContent from "./(pages)/_home";
+import HomeContent from "./_home";
 
 const Home = async ({ params: { lang } }: { params: { lang: Locale } }) => {
    return <HomeContent lang={lang} />;
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,29 @@
 import { MetadataRoute } from "next";
+import { Locale } from "@/i18n-config";
+import { getRouteArr } from "./[lang]/_libs/constants";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-   const baseURL = process.env.NEXT_PUBLIC_SITE_URL || "";
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+   const baseURL = (process.env.NEXT_PUBLIC_SITE_URL || "").replace(/\/$/, "");
 
-   return [
-      {
-         url: `${baseURL}`,
-         lastModified: new Date(),
-         changeFrequency: "yearly",
-         priority: 1,
-      },
-   ];
+   const lastModified = new Date();
+   const languages: Locale[] = ["ja", "en"];
+
+   /*===============================================
+	static pages
+	===============================================*/
+   const staticPages = getRouteArr();
+   const staticPaths: MetadataRoute.Sitemap = [];
+
+   languages.forEach((lang) => {
+      staticPages.forEach((page) => {
+         const href = page.href.replace(/^\/+/, "");
+         staticPaths.push({
+            url: `${baseURL}/${lang}/${href}`,
+            lastModified: lastModified,
+            changeFrequency: "yearly",
+         });
+      });
+   });
+
+   return [...staticPaths];
 }
