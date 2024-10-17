14d91cd6409135396caa0e20d2a28ed3f0584e6f
takuma-hmng8 <t.hashimoto@funtech.inc>
2024-10-17 12:25:55 +0900

    update

--- hooks/useGsapRegister.ts
+++ hooks/useGsapRegister.ts
@@ -1,8 +1,12 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ISDEV } from "@/app/[lang]/_libs/constants";
 
 export const useGsapRegister = () => {
    gsap.registerPlugin(ScrollTrigger);
+   ScrollTrigger.defaults({
+      markers: ISDEV,
+   });
    gsap.registerPlugin(useGSAP);
 };
